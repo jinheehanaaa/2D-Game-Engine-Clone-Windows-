cd5fae89c02cadef24067ed9acbf2f66cd69896d
Jinhee Han <jinheehanaaa@gmail.com>
2023-01-17 22:23:22 -0600

    Include SDL

---
+++ 2DGameEngine/src/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+
+// $(SolutionDir)
+int main(int argc, char* args[]) {
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+	std::cout << "Hello" << std::endl;
+	return 0;
+}
