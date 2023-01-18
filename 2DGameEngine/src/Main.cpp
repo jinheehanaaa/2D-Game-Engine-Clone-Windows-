0bc63dbeb9fa4d3c3e212bf733ff0601badfc933
Jinhee Han <jinheehanaaa@gmail.com>
2023-01-17 22:31:30 -0600

    Include Lua

--- 2DGameEngine/src/Main.cpp
+++ 2DGameEngine/src/Main.cpp
@@ -3,9 +3,15 @@
 #include <SDL_image.h>
 #include <SDL_ttf.h>
 #include <SDL_mixer.h>
+#include <glm/glm.hpp>
+#include <lua/sol.hpp>
+#include <lua/sol.hpp>
 
 // $(SolutionDir)
 int main(int argc, char* args[]) {
+	sol::state lua;
+	lua.open_libraries(sol::lib::base);
+	glm::vec2 velocity = glm::vec2(2.0, -1.0);
 
 	SDL_Init(SDL_INIT_EVERYTHING);
 	std::cout << "Hello" << std::endl;
