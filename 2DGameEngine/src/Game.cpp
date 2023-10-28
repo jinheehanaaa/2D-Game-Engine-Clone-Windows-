40bf8cd7e5fe2d621dcd076fb17bfd504713c272
Jinhee Han <80008300+jinheehanaaa@users.noreply.github.com>
2023-10-28 13:33:15 -0500

    007 - Maps and Tiles

--- 2DGameEngine/src/Game.cpp
+++ 2DGameEngine/src/Game.cpp
@@ -2,6 +2,7 @@
 #include "./Constants.h"
 #include "./Game.h"
 #include "./AssetManager.h"
+#include "./Map.h"
 #include "Components/TransformComponent.h"
 #include "./Components/SpriteComponent.h"
 #include "./Components/KeyboardControlComponent.h"
@@ -12,6 +13,7 @@ EntityManager manager;
 AssetManager* Game::assetManager = new AssetManager(&manager);
 SDL_Renderer* Game::renderer;
 SDL_Event Game::event;
+Map* map;
 
 Game::Game() {
     this->isRunning = false;
@@ -58,6 +60,10 @@ void Game::LoadLevel(int LevelNumber) {
     assetManager->AddTexture("tank-image", std::string("./assets/images/tank-big-right.png").c_str());
     assetManager->AddTexture("chopper-image", std::string("./assets/images/chopper-spritesheet.png").c_str());
     assetManager->AddTexture("radar-image", std::string("./assets/images/radar.png").c_str());
+    assetManager->AddTexture("jungle-tiletexture", std::string("./assets/tilemaps/jungle.png").c_str());
+
+    map = new Map("jungle-tiletexture", 1, 32);
+    map->LoadMap("./assets/tilemaps/jungle.map", 25, 20);
 
     // Start including entities and also components to them
     Entity& chopperEntity(manager.AddEntity("chopper"));
@@ -71,12 +77,9 @@ void Game::LoadLevel(int LevelNumber) {
     tankEntity.AddComponent<SpriteComponent>("tank-image");
 
 
-
     Entity& radarEntity(manager.AddEntity("Radar"));
     radarEntity.AddComponent<TransformComponent>(720, 15, 0, 0, 64, 64, 1);
     radarEntity.AddComponent<SpriteComponent>("radar-image", 8, 150, false, true); // frame, speed, direction, fixed
-
-
 }
 
 void Game::ProcessInput() {
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Objective 
 - Learn how to make game engine with C++, Lua, & SDL
-- This is not a How-To-Do Instruction (It is for reference purposes)
 
 # Technologies
 - C++
@@ -8,7 +7,11 @@
 - SDL for easy memory allocation from OS
 - Lua for scripting language
 
-# Important Branch
+# Folder Structure
+## Component Folder 
+- Fundamantal structure
+
+# Major Branch
 - [1. Setup completed for the first time in Windows OS](https://github.com/jinheehanaaa/2D-Game-Engine-with-CPP-LUA-and-SDL/tree/5679f7385963dd234a4953f9d47ce691cb4a083d)
 - [2. Update in Game Loop with proper delta time method](https://github.com/jinheehanaaa/2D-Game-Engine-with-CPP-LUA-and-SDL/tree/12638eb15da786823de46f9a7e3e2ae9cc1b4b4c)
 
@@ -20,12 +23,6 @@
 - [SDL-mixer](https://github.com/libsdl-org/SDL_mixer/releases)
 - [Lua-Binary](https://luabinaries.sourceforge.net/)
 
-# Resources for other projects
-- They teach common things about game engine (Game loop, delta time, tilemap, etc)
+# Resources for other similar projects
 - [Handmade Hero](https://www.youtube.com/@MollyRocket) for C Style Game Engine
 - [2D Game in Java](https://youtube.com/playlist?list=PL_QPQmz5C6WUF-pOQDsbsKbaBZqXj4qSq) for Making Java Game Engine without library
-
-
-# Private-Use-Only
-- [This URL](https://github.com/jinheehanaaa/2D-Game-Engine-with-CPP-LUA-and-SDL)
-- [Study Note](NOTE/note-taking/)
